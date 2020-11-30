f9c715df826c8915e03025a13901bb3a2220add7
Thierry <34653660+ThierryBARS@users.noreply.github.com>
2020-11-30 15:57:40 +0100

    Update Compteur_74_LS_393.ino

--- As an 8 bits counter/Compteur_74_LS_393.ino
+++ As an 8 bits counter/Compteur_74_LS_393.ino
@@ -20,17 +20,17 @@ Ground = 0v
 1A     = Digital I/O 13 qui sera en OUTPUT (elle fournira 
 les impulsions a compter). Dans le soft "A_Compter"
 
-1 CLR et 2 CLR seront reliées ensemble et reliées à
+1 CLR et 2 CLR seront reli�es ensemble et reli�es �
 la sortie Digital I/O 12, et portera le nom "CLR_compteur"
 
-1QD sera reliée sur 2A (pourtransformer les 2 compteurs 4bs
+1QD sera reli�e sur 2A (pourtransformer les 2 compteurs 4bs
 en 1 compteur 8 bits
 
 1QA, 1QB, 1QC, 1QD, 2QA, 2QB, 2QC, 2QD seront respectivement
-reliées aux Digitals I/O: 
+reli�es aux Digitals I/O: 
 2, 3, 4, 5, 6, 7.
-Je lai pas commencé par  digital I/O 0 et 1 car elles sont 
-utilisées par l'instruction serial en cas d'erreur de comptage
+Je lai pas commenc� par  digital I/O 0 et 1 car elles sont 
+utilis�es par l'instruction serial en cas d'erreur de comptage
 Il faut donc ouvrir le "moniteur serie" pour voir les erreurs
 
 */
@@ -38,9 +38,9 @@ Il faut donc ouvrir le "moniteur serie" pour voir les erreurs
 //---------------Sorties------------------------------------
 const int A_Compter =  13; // broche envoyant les impulsions a comp
                            // ter sur 1A du compteur.
-const int CLR_compteur =  12; // broche ou le clear est branché 
+const int CLR_compteur =  12; // broche ou le clear est branch� 
 
-//---------------Entrées------------------------------------
+//---------------Entr�es------------------------------------
 const int b0 =2;
 const int b1 =3;
 const int b2 =4;
@@ -54,23 +54,30 @@ const int BtnStartReset =  11; // broche du bouton reset test
 
 //---------------Variables du programme--------------------------------
 byte CompteurLogiciel; // Compteur logiciel du nombre d'impulsions 
-                      // envoyées sur 1A via Digital I/O 13
+                      // envoy�es sur 1A via Digital I/O 13
 byte LectureCompteur;  // Lecture des sorties 1Qx et 2Qx via les ports
-                      // PINB et PIND  (car les 8 broches s'étalent sur 
+                      // PINB et PIND  (car les 8 broches s'�talent sur 
                       // ces 2 ports du fait de ne pas utiliser Digital 
                       // I/O 0 et 1 
 
 
 void remise_a_zero_Compteur(){
-  digitalWrite(A_Compter, HIGH) ;
-  digitalWrite(A_Compter, LOW) ; 
+  digitalWrite(CLR_compteur, HIGH) ;
+  digitalWrite(CLR_compteur, LOW) ; 
   CompteurLogiciel=0;
-  }
+  
+      //lit, sous forme d'un octet, les ports PINB et PIND, n'ayant pas 
+      //utilis� les DigitalIO 0 et 1, j'empiete sur le PINB ET PIND 
+      // LectureCompteur=(((PINB)&3)<<6)|((PIND&252)>>2);
+
+  LectureCompteur=(((PINB)&3)<<6)|((PIND&252)>>2);
+}
 
 void incrementeCompteur(){
   digitalWrite(A_Compter, LOW) ; 
   digitalWrite(A_Compter, HIGH) ;
   CompteurLogiciel += 1;
+  LectureCompteur=(((PINB)&3)<<6)|((PIND&252)>>2);
 }
 
 
@@ -89,7 +96,8 @@ void setup() {
   pinMode(b6, INPUT);
   pinMode(b7, INPUT);
   pinMode(BtnStartReset, INPUT);
-  
+
+  delay(20);
   remise_a_zero_Compteur();
 }
 
@@ -97,13 +105,12 @@ void loop() {
 
     if (digitalRead(BtnStartReset) == 0) {
 
+      delay(125);
       incrementeCompteur();
-    
-      //lit, sous forme d'un octet, les ports PINB et PIND, n'ayant pas 
-      //utilisé les DigitalIO 0 et 1, j'empiete sur le PINB ET PIND 
-      LectureCompteur=(((PINB)&3)<<6)|((PIND&252)>>2);
+      
+   
 
-      //Ci dessous une méthode plus compréhensible
+      //Ci dessous une m�thode plus compr�hensible
       //LectureCompteur=digitalRead(b7)*128+digitalRead(b6)*64+digitalRead(b5)*32+digitalRead(b4)*16+digitalRead(b3)*8+digitalRead(b2)*4+digitalRead(b1)*2+digitalRead(b0);
     
       if (CompteurLogiciel != LectureCompteur) {
@@ -115,10 +122,8 @@ void loop() {
     }
      else if (digitalRead(BtnStartReset) == 1) {
       Serial.println("Remise a 0");
-      remise_a_zero_Compteur;
-      LectureCompteur=0;
-      delay(500);
-     }
+      remise_a_zero_Compteur();
+    }
     
       
 }
